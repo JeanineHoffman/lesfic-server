b928f4497ccad965ab9ee86ac1e9867e6482a623
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-08-17 10:47:55 -0700

    ready to create tables and migrate

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://Jeanine@localhost/lesfic',
+  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://Jeanine@localhost/lesfic-test',
+  API_ENDPOINT: `https://.herokuapp.com/`,
+API_KEY: process.env.REACT_APP_API_KEY,
 }
