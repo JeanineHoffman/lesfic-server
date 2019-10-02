0dc534f103dba745591caaf8393dab9963b4d589
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-10-01 17:47:50 -0700

    heroku fixes

--- src/postgrator-config.js
+++ src/postgrator-config.js
@@ -7,9 +7,9 @@ module.exports = {
     "driver": "pg",
     "host": process.env.MIGRATION_DB_HOST,
     "port": process.env.MIGRATION_DB_PORT,
-    "database": process.env.MIGRATION_DB_NAME,
-    "username": process.env.MIGRATION_DB_USER,
-    "password": process.env.MIGRATION_DB_PASS,
+    "database": process.env.MIGRATION_DATABASE_NAME,
+    "username": process.env.MIGRATION_DATABASE_USER,
+    // "password": process.env.MIGRATION_DB_PASS,
     "validateChecksums": false,
     "ssl": false
 }
--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 const app = require('./app')
 
-const { PORT, DB_URL } = require('./config')
+const { PORT, DATABASE_URL } = require('./config')
 // const PORT = process.env.PORT || 8000
 const knex = require('knex')
 
-const db = knex({ client: 'pg', connection: DB_URL, })
+const db = knex({ 
+  client: 'pg', 
+  connection: DATABASE_URL, 
+})
 
 app.set('db', db);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
+
+  module.exports = {app};
 })
